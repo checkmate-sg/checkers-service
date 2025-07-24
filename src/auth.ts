eb1a782de342f676bb2c85592e664c4ec7856a9a
zackeus <zchoo2001@gmail.com>
2025-07-24 16:01:30 +0800

    changed config file name

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 // src/auth.ts
 import NextAuth from "next-auth";
-import { authConfig } from "./lib/authConfig";
+import { authConfig } from "./lib/auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
