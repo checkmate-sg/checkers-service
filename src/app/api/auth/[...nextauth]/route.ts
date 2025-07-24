2512de72768055e51a08312412dd8d1371086770
zackeus <zchoo2001@gmail.com>
2025-07-24 16:06:18 +0800

    changed config file name

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,8 @@ export const runtime = "nodejs";
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth.config";
 
+console.log("[In Path]");
+
 const { handlers } = NextAuth(authConfig);
 
 export const { GET, POST } = handlers;
